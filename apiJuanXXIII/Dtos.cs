05d747d06e87fa7069233b7c2a37c4f994427f4c
oscarinmm <moreno.oscar.andres@gmail.com>
2022-11-07 01:57:54 -0300

    Arreglo

--- apiJuanXXIII/Dtos.cs
+++ apiJuanXXIII/Dtos.cs
@@ -2,9 +2,6 @@
 
 namespace apiJuanXXIII
 {
-    public record CargoDTO(int codigo, string nombre);
-    public record ComunaDTO(int codigo, string nombre);
-    public record ColegioDTO(string codigo, string nombre, string codigotipo, string comuna, string direccion, string telefono, string director, bool sector, int movilizacion, string texto, int colacion, string jornada, int pme);
     public record RegionesDTO(int id_region, string nombre_region);
     public record DepartamentoDTO(int cod_depto, string nombre_depto);
     public record SucursalesDTO(int cod_sucursal, string nombre_suc, int region, int depto, string jefe_suc);
